d73b4a763782d462b6de2996a476fd4dd6108fbd
Haripriya987 <aharipriya7093@gmail.com>
2023-09-25 23:04:34 +0530

    Day 30 programs are submitted

---
+++ Day 30/ClosestSmallElement.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ClosestSmallElement {
+    public static void closestSmallElement(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            int x=-1;
+            for(int j=i-1;j>-1;j--){
+                if(arr[i]>arr[j]){
+                    x=arr[j];
+                    break;
+                }
+            }
+            System.out.print(x+" ");
+        }
+        System.out.println();
+    }
+    //Right Side
+    public static void closestSmallElementRightSide(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int x = -1;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    x = arr[j];
+                    break;
+                }
+            }
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void closestSmallElementWithStack(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!st.isEmpty() && st.peek() > arr[i])
+                st.pop();
+
+            if(st.isEmpty())
+                System.out.print(-1+" ");
+            else
+                System.out.print(st.peek()+" ");
+            st.push(arr[i]);
+        }
+        System.out.println();
+    }
+
+
+    static void stockSpan(int[] arr){
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] < arr[i])
+                s.pop();
+
+            if(s.isEmpty())
+                System.out.print(i+1+" ");
+            else
+                System.out.print(i-s.peek()+" ");
+            s.push(i);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+
+        int[] arr={2,3,5,1,6};
+        closestSmallElement(arr);
+        closestSmallElementWithStack(arr);
+        stockSpan(arr);
+        closestSmallElementRightSide(arr);
+
+    }
+}
