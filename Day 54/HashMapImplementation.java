4eec621a38a61858508c84ebf48925fe2d3f50bd
Haripriya987 <aharipriya7093@gmail.com>
2023-10-20 22:27:47 +0530

    Day 54 programs are submitted

---
+++ Day 54/HashMapImplementation.java
@@ -0,0 +1,55 @@
+import java.util.LinkedList;
+
+public class HashMapImplementation {
+    public static void main(String[] args){
+        HashTable ht=new HashTable();
+        ht.put(32,"Hello");
+        ht.put(12,"Hi");
+        System.out.println(ht.get(2));
+    }
+
+}
+class HashTable{
+    private class Entry {
+        int key;
+        String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+        private LinkedList<Entry>[] entries=new LinkedList[5];
+        private int hash(int key){
+            return key%entries.length;
+        }
+        public void put(int key,String value){
+            int index=hash(key);
+            if(entries[index]==null)
+                entries[index]=new LinkedList<>();
+
+            var bucket=entries[index];
+            for(var entry:bucket){
+                if(entry.key==key){
+                    entry.value=value;
+                    return;
+                }
+            }
+
+            var entry=new Entry(key, value);
+            entries[index].addLast(entry);
+        }
+        public String get(int key){
+            int index=hash(key);
+            var bucket=entries[index];
+            if(bucket!=null){
+                for(var entry:bucket){
+                    if(entry.key==key)
+                        return entry.value;
+                }
+            }
+            return null;
+        }
+
+
+}
