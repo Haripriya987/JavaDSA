7ec84f12f392b0e4daf7f6e8a78062609fa16542
Haripriya987 <aharipriya7093@gmail.com>
2023-11-08 20:38:04 +0530

    Day 71 programs are submitted

---
+++ Day 71/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+    BSTTree bst=new BSTTree();
+    bst.insert(32);
+    bst.insert(12);
+    bst.insert(65);
+    bst.insert(85);
+    bst.insert(21);
+    bst.insert(10);
+    bst.insert(3);
+    System.out.println(bst.search(45));
+    bst.inorderT(bst.root);
+    System.out.println();
+    bst.postorderT(bst.root);
+    System.out.println();
+    bst.preorderT(bst.root);
+        System.out.println();
+        bst.printLevelNodes(bst.root,3);
+        System.out.println();
+        bst.printNodesLevelWise(bst.root);
+        System.out.println();
+        bst.printLRChild(bst.root);
+        System.out.println();
+        System.out.println(bst.noOfLeaf(bst.root));
+        System.out.println(bst.sizeOfTree(bst.root));
+        bst.printLeafNodes(bst.root);
+
+//        System.out.println(bst.max(bst.root));
+//        System.out.println(bst.min(bst.root));
+//        System.out.println(bst.leftChildNode(12));
+//        System.out.println(bst.rightChildNode(12));
+//        System.out.println(bst.leftGrandChildNode(32));
+//        System.out.println(bst.rightGrandChildNode(32));
+//        System.out.println(bst.height(bst.root));
+//        System.out.println(bst.noOfLeaf(bst.root));
+//        System.out.println(bst.sizeOfTree(bst.root));
+    }
+}
